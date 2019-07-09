3e5fb7e60ab6a247c556cb9ddda4182a1a3c85c1
minhaj <ahmedminhaj44@gmail.com>
2019-07-10 01:09:43 +0600

    Todo list app done

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the Todo list app. Which develop with ReactJS and basis CSS.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
